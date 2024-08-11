9ef57ed8bf75d7fce61b85fd6676f40a72cab857
Elwood Nelson <elwoodnelson@gmail.com>
2024-08-10 20:08:01 -0500

    like button works

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -4,12 +4,35 @@ const pool = require('../modules/pool.js');
 
 // PUT /gallery/like/:id
 router.put('/like/:id', (req, res) => {
-  // code here
+  const change = req.body.change;
+  const itemIdToChange = req.params.id;
+
+  let sqlText;
+  if(change === 1){
+    sqlText = `UPDATE "gallery"
+                SET "likes" = "likes" + 1
+                WHERE "id" = $1;`
+  }
+  else {
+    sqlText = `UPDATE "gallery"
+    SET "likes" = "likes" - 1
+    WHERE "id" = $1;`
+  }
+  sqlValue = [itemIdToChange];
+  pool.query(sqlText, sqlValue)
+  .then((dbResult) =>{
+    res.sendStatus(200)
+  })
+  .catch((dbError)=>{
+    console.log('error updating database likes',dbError);
+    res.sendStatus(500)
+  })
 });
 
 // GET /gallery
 router.get('/', (req, res) => {
-  const queryText = `SELECT * from "gallery"`
+  const queryText = `SELECT * from "gallery"
+                    ORDER BY "id";`
   pool.query(queryText)
     .then((result) => {
     res.send(result.rows);
